fb3aaab3ecd70ef3f253345009d4a99b8f2eed2d
pizzapie000 <pizza@rshike.com>
2025-01-18 11:16:23 -0700

    modified:   csharp-prep/Prep1/Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Get the first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Get the last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Show the text James Bond style!
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
