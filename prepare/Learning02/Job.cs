36d932838b79e9919b7cbbb94ab40a89e9174d3f
pizzapie000 <pizza@rshike.com>
2025-01-21 16:53:35 -0700

    new file:   prepare/Learning02/Job.cs 	modified:   prepare/Learning02/Program.cs 	new file:   prepare/Learning02/Resume.cs

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+public class Job
+{
+    // Member variables
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    // Display job details
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
